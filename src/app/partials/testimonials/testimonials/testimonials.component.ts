ddfa87e8267004b5598cc37076026c5eff72ef06
Joewings-jw <ngugijoseph830@gmail.com>
2021-06-14 22:42:00 +0300

    swiper config

--- src/app/partials/testimonials/testimonials/testimonials.component.ts
+++ src/app/partials/testimonials/testimonials/testimonials.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-testimonials',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestimonialsComponent implements OnInit {
 
+  config: SwiperOptions = {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 100,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    breakpoints: {
+      568:{
+        width:568,
+        slidesPerView: 1,
+      },
+      768:{
+        width:768,
+        slidesPerView: 2,
+      },
+      1024:{
+        width: 1024,
+        slidesPerView: 2,
+      }
+    }
+      
+      
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
 }
+
